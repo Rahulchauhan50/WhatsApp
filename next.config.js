12e067c53964ec26084ebc57628b88aa18da5717
Rahul chauhan <rahulchauhah50@gmail.com>
2026-03-29 14:00:33 +0530

    Remove .env from tracking

--- next.config.js
+++ next.config.js
@@ -1,12 +1,31 @@
 /** @type {import('next').NextConfig} */
+const imageDomains = process.env.IMAGE_DOMAINS
+  ? process.env.IMAGE_DOMAINS.split(',')
+  : [];
+
 const nextConfig = {
   reactStrictMode: false,
   env:{
     NEXT_PUBLIC_ZEGO_APP_ID:"366589160",
     NEXT_PUBLIC_ZEGO_SERVER_ID:"b53e2b1c2246bbb3626d71b9bc55fdb8"
   },
-  images: {
-    domains: process.env.IMAGE_DOMAINS ? process.env.IMAGE_DOMAINS.split(',') : [],
+    images: {
+    remotePatterns: [
+      ...imageDomains.map((domain) => ({
+        protocol: 'https',
+        hostname: domain.trim(),
+        port: '',
+        pathname: '/**',
+      })),
+
+      // ✅ Explicit rule for localhost (your missing piece)
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '5000',
+        pathname: '/**',
+      },
+    ],
   },
   async headers() {
     return [
